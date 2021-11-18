e5b55fdd9d3d51e3753889b6f9bc3ce45c8c878a
TBelizario <thiago_belizario@hotmail.com>
2021-11-18 20:31:00 -0300

    Monitor de sites

--- Monitor_de_Sites.go
+++ Monitor_de_Sites.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,6 +32,7 @@ func main() {
 			iniciaMonitoramento()
 		case 2:
 			fmt.Println("Gerando Logs...")
+			mostrarLogs()
 		default:
 			os.Exit(-1)
 		}
@@ -53,7 +59,8 @@ func solicitaMenu() int {
 
 func iniciaMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.youtube.com/", "https://www.google.com.br/", "https://golang.org/"}
+	siteTxt := "sites.txt"
+	sites := leArquivos(siteTxt)
 
 	for i := 0; i < qtdVezes; i++ {
 		for i, site := range sites {
@@ -69,7 +76,50 @@ func statusSites(site string) {
 	resp, _ := http.Get(site)
 	if resp.StatusCode == 200 {
 		fmt.Println("O site Esta funcionando perfeitamente")
+		log(site, true)
 	} else {
 		fmt.Println("O site Esta com problemas")
+		log(site, false)
 	}
+}
+
+func leArquivos(site string) []string {
+	var sites []string
+
+	arquivo, err := os.Open(site)
+	if err != nil {
+		fmt.Println("Erro detectado: ", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+	arquivo.Close()
+	return sites
+}
+
+func log(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Printf("Falha ao ler ou para criar o arquivo")
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - Online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func mostrarLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo")
+	}
+	println(string(arquivo))
 }
